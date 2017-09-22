8fb1c5518bda873bc2d07d16bcf3cd833d5bcd38
John <iplanetcn@gmail.com>
2017-09-22 13:16:22 +0800

    add macosx uninstall jdk9

---
+++ java/README.md
@@ -0,0 +1,5 @@
+# uninstall jdk9
+execute blow script:
+```shell
+ bash <(curl -s https://raw.githubusercontent.com/iplanetcn/script/master/java/macosx_remove_java9.sh)
+```
---
+++ java/macosx_remove_java9.sh
@@ -0,0 +1,4 @@
+sudo rm -fr /Library/Java/JavaVirtualMachines/jdk-9.jdk/
+sudo rm -fr /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
+sudo rm -fr /Library/PreferencePanes/JavaControlPanel.prefPane
+
